ae9ea9288355dfd4a758fe8037205cf38e8f0f9a
AidenCze <85247269+AidenCze@users.noreply.github.com>
2026-09-20 21:54:53 +0200

    UI V1

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,24 +1,26 @@
 """
 URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.forms import AuthenticationForm
+
+# Úprava výchozího formuláře pro přidání Bootstrap CSS tříd
+class CustomLoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    # Předání vlastního formuláře do LoginView
+    path('login/', auth_views.LoginView.as_view(
+        template_name='login.html',
+        authentication_form=CustomLoginForm
+    ), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
